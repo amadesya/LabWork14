341138372a142e7d43c208dd8e7d121bbaf73df0
amadesya <ultrashitband@gmail.com>
2024-10-25 13:27:40 +0300

    Добавьте файлы проекта.

---
+++ DBLibrary/DAL.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DBLibrary
+{
+    public static class DAL
+    {
+        private static string _server = @"PRSERVER\SQLEXPRESS";
+        private static string _database = "ispp3512";
+        private static string _login = "ispp3512";
+        private static string _password = "3512";
+
+        public static string ConnectionString
+        {
+            get
+            {
+                SqlConnectionStringBuilder builder = new()
+                {
+                    DataSource = _server,
+                    InitialCatalog = _database,
+                    UserID = _login,
+                    Password = _password,
+                    TrustServerCertificate = true
+                };
+
+                return builder.ConnectionString;
+            }
+        }
+
+        public static async Task ChangeConnectionSettings(string _server, string _database, string _login, string _password)
+        {
+            using SqlConnection connection = new(ConnectionString);
+            await connection.OpenAsync();
+        }
+    }
+}
---
+++ LabWork14/Program.cs
@@ -0,0 +1,7 @@
+﻿using DBLibrary;
+
+
+Console.WriteLine(DAL.ConnectionString);
+
+await DAL.ChangeConnectionSettings(@"PRSERVER\SQLEXPRESS","ispp3511","ispp3511","3511");
+Console.WriteLine(DAL.ConnectionString);
