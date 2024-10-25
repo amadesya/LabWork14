1e32977579ea75ba63e77427aedfc14be93b1e1b
amadesya <ultrashitband@gmail.com>
2024-10-25 14:22:41 +0300

    Tasks 1, 2, 3 and 4

--- DBLibrary/DAL.cs
+++ DBLibrary/DAL.cs
@@ -26,10 +26,44 @@ namespace DBLibrary
             }
         }
 
-        public static async Task ChangeConnectionSettings(string _server, string _database, string _login, string _password)
+        public static async Task ChangeConnectionSettings(string server, string database, string login, string password)
+        {
+            _server = server;
+            _database = database;
+            _login = login;
+            _password = password;
+        }
+
+        public static async Task<bool> CheckConnectivity()
+        {
+            try
+            {
+                using SqlConnection connection = new(ConnectionString);
+                await connection.OpenAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static async Task<int> CompleteCommand(string query)
         {
             using SqlConnection connection = new(ConnectionString);
             await connection.OpenAsync();
+
+            SqlCommand command = new(query, connection);
+            return command.ExecuteNonQuery();
+        }
+
+        public static async Task<object?> GetObject(string query)
+        {
+            using SqlConnection connection = new(ConnectionString);
+            await connection.OpenAsync();
+
+            SqlCommand command = new(query, connection);
+            return command.ExecuteScalarAsync();
         }
     }
 }
--- LabWork14/Program.cs
+++ LabWork14/Program.cs
@@ -1,7 +1,28 @@
 ﻿using DBLibrary;
+using System.Collections.Generic;
+using System.Diagnostics;
 
-
+//Task 5.1
+Console.WriteLine("Task 5.1\n");
 Console.WriteLine(DAL.ConnectionString);
 
+//Task 5.2.1
+Console.WriteLine("\nTask 5.2.1");
 await DAL.ChangeConnectionSettings(@"PRSERVER\SQLEXPRESS","ispp3511","ispp3511","3511");
 Console.WriteLine(DAL.ConnectionString);
+
+//Task 5.2.2
+Console.WriteLine("\nTask 5.2.2");
+bool isConnection = await DAL.CheckConnectivity();
+Console.WriteLine(isConnection);
+
+
+//Task 5.3
+Console.WriteLine("\nTask 5.3");
+var rows = await DAL.CompleteCommand("UPDATE Game SET Price = 150 WHERE GameId = 3");
+Console.WriteLine($"Количество изменённых строк: {rows}");
+
+//Task 5.4
+Console.WriteLine("\nTask 5.4");
+object query = await DAL.GetObject("UPDATE Game SET Price = 150 WHERE GameId = 3");
+Console.WriteLine($"Изменённый объект: {query}");
